6e52cccd63b9efbc97c4bc3e6e0242af5a80d8f0
flutteruser <ghetrung1@gmail.com>
2026-02-21 14:36:23 +0700

    check folder and factory

--- lib/screens/transaction_list_screen.dart
+++ lib/screens/transaction_list_screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:mamoney/services/transaction_provider.dart';
 import 'package:mamoney/services/auth_provider.dart';
 import 'package:intl/intl.dart';
-import 'package:mamoney/utils/currency_utils.dart';
 
 class TransactionListScreen extends StatefulWidget {
   const TransactionListScreen({super.key});
